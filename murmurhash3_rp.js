2e6641ee3c1ae991d3c17c4b21abc0d4803e4116
Roland Pihlakas <roland@pinal.ee>
2015-01-25 11:46:01 +0200

    Update murmurhash3_rp.js

--- murmurhash3_rp.js
+++ murmurhash3_rp.js
@@ -13,18 +13,20 @@
 
     function murmurhash3_32_rp(key, seed) {
 
-        var keyLength, tailLength, bodyLength, h1, k1, i, c1_low, c1_high, c2_low, c2_high;
+        var keyLength, tailLength, tailLength4, bodyLength, bodyLength8, h1, k1, i, c1, c1_low, c1_high, c2, c2_low, c2_high, k1B;
 
         keyLength = key.length;
         tailLength = keyLength & 3;
         bodyLength = keyLength - tailLength;
+        tailLength4 = bodyLength & 7;
+        bodyLength8 = bodyLength - tailLength4;
         h1 = seed;
 
-        //c1 = 0xcc9e2d51;
+        c1 = 0xcc9e2d51;
         c1_low = 0x2d51;
         c1_high = 0xcc9e0000;
 
-        //c2 = 0x1b873593;
+        c2 = 0x1b873593;
         c2_low = 0x3593;
         c2_high = 0x1b870000;
 
@@ -34,16 +36,65 @@
 
         i = 0;
 
-        while (i < bodyLength) {
+        while (i < bodyLength8) {
 
             k1 =
-              ((key.charCodeAt(i) & 0xff)) |
-              ((key.charCodeAt(++i) & 0xff) << 8) |
-              ((key.charCodeAt(++i) & 0xff) << 16) |
-              ((key.charCodeAt(++i) & 0xff) << 24);
+                ((key.charCodeAt(i) & 0xff)) |
+                ((key.charCodeAt(++i) & 0xff) << 8) |
+                ((key.charCodeAt(++i) & 0xff) << 16) |
+                ((key.charCodeAt(++i) & 0xff) << 24);
+            
+            k1B =
+                ((key.charCodeAt(++i) & 0xff)) |
+                ((key.charCodeAt(++i) & 0xff) << 8) |
+                ((key.charCodeAt(++i) & 0xff) << 16) |
+                ((key.charCodeAt(++i) & 0xff) << 24);
+            
+            ++i;
+            
+
+            //k1 *= c1;
+            k1 = (c1_high * k1 | 0) + (c1_low * k1);
+            //k1 = ROTL32(k1,15);
+            k1 = (k1 << 15) | (k1 >>> 17);
+            //k1 *= c2;
+            k1 = (c2_high * k1 | 0) + (c2_low * k1);
+    
+            //k1 *= c1;
+            k1B = (c1_high * k1B | 0) + (c1_low * k1B);
+            //k1 = ROTL32(k1,15);
+            k1B = (k1B << 15) | (k1B >>> 17);
+            //k1 *= c2;
+            k1B = (c2_high * k1B | 0) + (c2_low * k1B);
+
+            
+            //h1 ^= k1;
+            h1 ^= k1;
+            //h1 = ROTL32(h1,13); 
+            h1 = (h1 << 13) | (h1 >>> 19);
+            //h1 = h1*5+0xe6546b64;
+            h1 = h1 * 5 + 0xe6546b64;
+            
+            //h1 ^= k1;
+            h1 ^= k1B;
+            //h1 = ROTL32(h1,13); 
+            h1 = (h1 << 13) | (h1 >>> 19);
+            //h1 = h1*5+0xe6546b64;
+            h1 = h1 * 5 + 0xe6546b64;
+
+        }   //while (i < bodyLength8) {
+
+        
+        if (tailLength4) {
+
+            k1 =
+                ((key.charCodeAt(i) & 0xff)) |
+                ((key.charCodeAt(++i) & 0xff) << 8) |
+                ((key.charCodeAt(++i) & 0xff) << 16) |
+                ((key.charCodeAt(++i) & 0xff) << 24);
 
             ++i;
-
+                
             //k1 *= c1;
             k1 = (c1_high * k1 | 0) + (c1_low * k1);
             //k1 = ROTL32(k1,15);
@@ -57,8 +108,8 @@
             h1 = (h1 << 13) | (h1 >>> 19);
             //h1 = h1*5+0xe6546b64;
             h1 = h1 * 5 + 0xe6546b64;
-
-        }   //while (i < bodyLength) {
+            
+        }   //if (tailLength4) {
 
 
         //----------
